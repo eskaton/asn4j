d6f2bb182a1bd2be87717621346df1d540d26a60
Eskaton <me@eskaton.ch>
2018-01-10 20:04:33 +0100

    Revert inadvertent changes

--- asn4j-tests/src/test/java/ch/eskaton/asn4j/test/x680_27/TestX680_27.java
+++ asn4j-tests/src/test/java/ch/eskaton/asn4j/test/x680_27/TestX680_27.java
@@ -27,16 +27,16 @@
 
 package ch.eskaton.asn4j.test.x680_27;
 
-import static org.junit.Assert.assertEquals;
-
-import ch.eskaton.asn4jtest.x680_27_implicit.TestSet2;
-import org.junit.Test;
-
 import ch.eskaton.asn4j.runtime.BERDecoder;
 import ch.eskaton.asn4j.runtime.BEREncoder;
 import ch.eskaton.asn4j.runtime.types.ASN1Integer;
 import ch.eskaton.asn4j.runtime.types.ASN1OctetString;
-import ch.eskaton.asn4jtest.x680_27_implicit.TestSet1;
+
+import ch.eskaton.asn4jtest.x680_27.TestSet1;
+import ch.eskaton.asn4jtest.x680_27.TestSet2;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class TestX680_27 {
 
