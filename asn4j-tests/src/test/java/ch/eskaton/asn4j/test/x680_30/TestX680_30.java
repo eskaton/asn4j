09c72a12524f0d3c9497b50af30149ec5013aff6
me@eskaton.ch <me@eskaton.ch>
2018-12-28 13:23:08 +0100

    Added test for selection types

---
+++ asn4j-tests/src/test/java/ch/eskaton/asn4j/test/x680_30/TestX680_30.java
@@ -0,0 +1,64 @@
+/*
+ *  Copyright (c) 2015, Adrian Moser
+ *  All rights reserved.
+ * 
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the author nor the
+ *  names of its contributors may be used to endorse or promote products
+ *  derived from this software without specific prior written permission.
+ * 
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL AUTHOR BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package ch.eskaton.asn4j.test.x680_30;
+
+import ch.eskaton.asn4j.runtime.BERDecoder;
+import ch.eskaton.asn4j.runtime.BEREncoder;
+import ch.eskaton.asn4j.runtime.types.ASN1Integer;
+import ch.eskaton.asn4j.runtime.types.ASN1OctetString;
+import ch.eskaton.asn4j.runtime.types.ASN1VisibleString;
+import ch.eskaton.asn4jtest.x680_27.TestSet1;
+import ch.eskaton.asn4jtest.x680_27.TestSet2;
+import ch.eskaton.asn4jtest.x680_30.TestSelectionSetType;
+import org.junit.Test;
+
+import static ch.eskaton.asn4jtest.x680_30.TestSelectionSetType.*;
+import static org.junit.Assert.assertEquals;
+
+public class TestX680_30 {
+
+    @Test
+    public void testTestSelectionSetType() {
+        TestSelectionSetType a = new TestSelectionSetType();
+        a.setA(ASN1Integer.valueOf(4711));
+        a.setB(ASN1VisibleString.fromString("test"));
+
+        SelectedType1 val = new SelectedType1();
+        val.setValue(true);
+
+        a.setC(val);
+
+        BEREncoder encoder = new BEREncoder();
+        BERDecoder decoder = new BERDecoder();
+
+        TestSelectionSetType b = decoder.decode(TestSelectionSetType.class, encoder.encode(a));
+
+        assertEquals(a, b);
+    }
+
+}
