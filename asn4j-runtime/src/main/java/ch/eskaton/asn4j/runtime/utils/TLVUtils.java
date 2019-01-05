280768b2438a66d4ee66ed08572efd28fa35250d
me@eskaton.ch <me@eskaton.ch>
2019-01-05 01:16:32 +0100

    Fixed bug when decoding tags

--- asn4j-runtime/src/main/java/ch/eskaton/asn4j/runtime/utils/TLVUtils.java
+++ asn4j-runtime/src/main/java/ch/eskaton/asn4j/runtime/utils/TLVUtils.java
@@ -1,7 +1,7 @@
 /*
  *  Copyright (c) 2015, Adrian Moser
  *  All rights reserved.
- * 
+ *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions are met:
  *  * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  *  * Neither the name of the author nor the
  *  names of its contributors may be used to endorse or promote products
  *  derived from this software without specific prior written permission.
- * 
+ *
  *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -30,7 +30,6 @@ package ch.eskaton.asn4j.runtime.utils;
 import ch.eskaton.asn4j.runtime.annotations.ASN1Tag;
 import ch.eskaton.asn4j.runtime.exceptions.EncodingException;
 import ch.eskaton.asn4j.runtime.types.ASN1Type;
-import ch.eskaton.asn4j.runtime.utils.RuntimeUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -42,8 +41,8 @@ public class TLVUtils {
 
     private static final double LOG10_2_DIV_8 = Math.log10(2) / 8;
 
-      public static byte[] getTagLength(ASN1Type obj, int contentLen)
-    		throws EncodingException {
+    public static byte[] getTagLength(ASN1Type obj, int contentLen)
+            throws EncodingException {
         List<ASN1Tag> tags = RuntimeUtils.getTags(obj.getClass());
 
         return getTagLength(tags, contentLen);
@@ -101,47 +100,46 @@ public class TLVUtils {
     }
 
     public static byte[] getLength(int contentLen) {
-    	byte[] buf;
+        byte[] buf;
 
-    	if (contentLen > 127) {
-    		int len = (int) Math.ceil(Math.log10(contentLen + 1)
-    				/ LOG10_2_DIV_8);
-    		buf = new byte[len + 1];
-    		buf[0] = (byte) (len & 0x7f | 0x80);
+        if (contentLen > 127) {
+            int len = (int) Math.ceil(Math.log10(contentLen + 1) / LOG10_2_DIV_8);
+            buf = new byte[len + 1];
+            buf[0] = (byte) (len & 0x7f | 0x80);
 
-    		for (int i = len; i > 0; i--) {
-    			buf[i] = (byte) (contentLen & 0xFF);
-    			contentLen >>= 8;
-    		}
-    	} else {
-    		buf = new byte[] { (byte) (contentLen & 0x7f) };
-    	}
+            for (int i = len; i > 0; i--) {
+                buf[i] = (byte) (contentLen & 0xFF);
+                contentLen >>= 8;
+            }
+        } else {
+            buf = new byte[] { (byte) (contentLen & 0x7f) };
+        }
 
-    	return buf;
+        return buf;
     }
 
     public static byte[] getTag(ASN1Tag tag) {
-    	byte[] buf;
-    	int tagNum = tag.tag();
+        byte[] buf;
+        int tagNum = tag.tag();
 
-    	if (tagNum > 30) {
-    		int len = (int) Math.ceil(Math.log10(tagNum + 1) / Math.log10(2)
-    				/ 7);
-    		buf = new byte[len + 1];
-    		buf[0] |= 0x1f;
-    		for (int i = len; i > 0; i--) {
-    			buf[i] = (byte) ((tagNum & 0x7f) | 0x80);
-    			tagNum >>= 7;
-    		}
-    		buf[len] &= ~0x80;
-    	} else {
-    		buf = new byte[] { (byte) (tagNum & 0x1f) };
-    	}
+        if (tagNum > 30) {
+            int len = (int) Math.ceil(Math.log10(tagNum + 1) / Math.log10(2) / 7);
+            buf = new byte[len + 1];
+            buf[0] |= 0x1f;
 
-    	buf[0] |= (byte) ((tag.clazz().ordinal() & 0x3) << 6)
-    			| (byte) ((tag.constructed() ? 1 : 0) << 5);
+            for (int i = len; i > 0; i--) {
+                buf[i] = (byte) ((tagNum & 0x7f) | 0x80);
+                tagNum >>= 7;
+            }
 
-    	return buf;
+            buf[len] &= ~0x80;
+        } else {
+            buf = new byte[] { (byte) (tagNum & 0x1f) };
+        }
+
+        buf[0] |= (byte) ((tag.clazz().ordinal() & 0x3) << 6) | (byte) ((tag.constructed() ? 1 : 0) << 5);
+
+        return buf;
     }
 
 }
