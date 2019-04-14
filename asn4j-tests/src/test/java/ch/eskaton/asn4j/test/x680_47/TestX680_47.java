f9e69ca625a7245d7cbc107b34397eec49cd9d21
me@eskaton.ch <me@eskaton.ch>
2019-04-14 20:38:19 +0200

    Added test for UTCTime

---
+++ asn4j-tests/src/test/java/ch/eskaton/asn4j/test/x680_47/TestX680_47.java
@@ -0,0 +1,82 @@
+/*
+ *  Copyright (c) 2015, Adrian Moser
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *  * Redistributions of source code must retain the above copyright
+ *  notice, this list of conditions and the following disclaimer.
+ *  * Redistributions in binary form must reproduce the above copyright
+ *  notice, this list of conditions and the following disclaimer in the
+ *  documentation and/or other materials provided with the distribution.
+ *  * Neither the name of the author nor the
+ *  names of its contributors may be used to endorse or promote products
+ *  derived from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ *  ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL AUTHOR BE LIABLE FOR ANY
+ *  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package ch.eskaton.asn4j.test.x680_47;
+
+import ch.eskaton.asn4j.runtime.BERDecoder;
+import ch.eskaton.asn4j.runtime.BEREncoder;
+import ch.eskaton.asn4j.runtime.exceptions.ASN1RuntimeException;
+import ch.eskaton.asn4jtest.x680_47.TestUTCTime;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestX680_47 {
+
+    @Test
+    public void testUTCTimeLocal() throws ASN1RuntimeException {
+        TestUTCTime a = new TestUTCTime();
+
+        a.setValue("1812311507");
+
+        BEREncoder encoder = new BEREncoder();
+        BERDecoder decoder = new BERDecoder();
+
+        TestUTCTime b = decoder.decode(TestUTCTime.class, encoder.encode(a));
+
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void testUTCTimeZulu() throws ASN1RuntimeException {
+        TestUTCTime a = new TestUTCTime();
+
+        a.setValue("181231150703Z");
+
+        BEREncoder encoder = new BEREncoder();
+        BERDecoder decoder = new BERDecoder();
+
+        TestUTCTime b = decoder.decode(TestUTCTime.class, encoder.encode(a));
+
+        assertEquals(a, b);
+    }
+
+    @Test
+    public void testUTCTimeOffset() throws ASN1RuntimeException {
+        TestUTCTime a = new TestUTCTime();
+
+        a.setValue("181231150703-1230");
+
+        BEREncoder encoder = new BEREncoder();
+        BERDecoder decoder = new BERDecoder();
+
+        TestUTCTime b = decoder.decode(TestUTCTime.class, encoder.encode(a));
+
+        assertEquals(a, b);
+    }
+
+}
