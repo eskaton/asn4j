c4aec891b5c7c9fb76dcc380310906f24b16feb7
me@eskaton.ch <me@eskaton.ch>
2019-04-14 17:52:01 +0200

    Extracted common code to AbstractDateTimeParser

--- asn4j-runtime/src/main/java/ch/eskaton/asn4j/runtime/parsing/GeneralizedTimeParser.java
+++ asn4j-runtime/src/main/java/ch/eskaton/asn4j/runtime/parsing/GeneralizedTimeParser.java
@@ -32,15 +32,9 @@ import ch.eskaton.asn4j.runtime.exceptions.ASN1RuntimeException;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class GeneralizedTimeParser {
-
-    public static final Integer ZERO = Integer.valueOf(0);
+public class GeneralizedTimeParser extends AbstractDateTimeParser {
 
     public static final BigDecimal SIXTY = BigDecimal.valueOf(60);
-
-    private enum State {
-        Initial, Year, Month, Day, Hour, Minute, Second, HourFraction, MinuteFraction, SecondFraction, TimeZone, Accept, Error
-    }
 
     public GeneralizedTimeParser() {
     }
@@ -125,6 +119,11 @@ public class GeneralizedTimeParser {
         }
     }
 
+    protected Integer parseSecond(Context ctx) throws IOException, ASN1RuntimeException {
+        return parseComponent(ctx, State.SecondFraction, 2, second ->
+                new LessEqualVerifiyer("second", 59).verify(second));
+    }
+
     private void processFraction(Context ctx, State state, String fractionStr) {
         BigDecimal fraction = new BigDecimal("0." + fractionStr);
         BigDecimal componentWithFraction;
@@ -145,35 +144,6 @@ public class GeneralizedTimeParser {
                 throw new IllegalStateException(ctx.getState().toString());
         }
 
-    }
-
-    private Integer parseYear(Context ctx) throws IOException, ASN1RuntimeException {
-        return parseComponent(ctx, State.Year, 4, null);
-    }
-
-    private Integer parseMonth(Context ctx) throws IOException, ASN1RuntimeException {
-        return parseComponent(ctx, State.Month, 2, month ->
-                new LessEqualVerifiyer("month", 12).verify(month));
-    }
-
-    private Integer parseDay(Context ctx) throws IOException, ASN1RuntimeException {
-        return parseComponent(ctx, State.Day, 2, day ->
-                new LessEqualVerifiyer("day", 31).verify(day));
-    }
-
-    private Integer parseHour(Context ctx) throws IOException, ASN1RuntimeException {
-        return parseComponent(ctx, State.Hour, 2, hour ->
-                new LessEqualVerifiyer("hour", 23).verify(hour));
-    }
-
-    private Integer parseMinute(Context ctx) throws IOException, ASN1RuntimeException {
-        return parseComponent(ctx, State.Minute, 2, minute ->
-                new LessEqualVerifiyer("minute", 59).verify(minute));
-    }
-
-    private Integer parseSecond(Context ctx) throws IOException, ASN1RuntimeException {
-        return parseComponent(ctx, State.SecondFraction, 2, second ->
-                new LessEqualVerifiyer("second", 59).verify(second));
     }
 
     private String parseFraction(Context ctx) throws IOException {
@@ -240,145 +210,15 @@ public class GeneralizedTimeParser {
             return sb.toString();
         }
 
-        int intMinutes = Integer.parseInt(minute);
+        int intMinute = Integer.parseInt(minute);
 
-        if (Integer.parseInt(hour) * 100 + intMinutes > 1800 || intMinutes > 59) {
+        if (Integer.parseInt(hour) * 100 + intMinute > 1800 || intMinute > 59) {
             throw new ASN1RuntimeException("Invalid offset");
         }
 
         sb.append(minute);
 
         return sb.toString();
-    }
-
-    private Integer parseComponent(Context ctx, State targetState, int length, Verifier<Integer> verifier) throws IOException, ASN1RuntimeException {
-        String component = parseDigits(ctx, length);
-
-        if (component != null) {
-            if (verifier != null) {
-                verifier.verify(Integer.valueOf(component));
-            }
-
-            ctx.setState(targetState);
-
-            return Integer.valueOf(component);
-        }
-
-        ctx.setState(State.Error);
-
-        return null;
-    }
-
-    private String parseDigits(Context ctx, int count) throws IOException {
-        StringBuilder sb = new StringBuilder();
-
-        ctx.mark();
-
-        for (int i = 0; i < count; i++) {
-            int digit = parseDigit(ctx);
-
-            if (digit == -1) {
-                ctx.reset();
-                return null;
-            }
-
-            sb.append((char) digit);
-        }
-
-        return sb.toString();
-    }
-
-    private int parseDigit(Context ctx) throws IOException {
-        int digit = ctx.read();
-
-        if (Character.isDigit(digit)) {
-            return digit;
-        }
-
-        ctx.unread();
-
-        return -1;
-    }
-
-    private static class Context {
-
-        private LexerInputStream is;
-
-        private State state = State.Initial;
-
-        Integer year = null;
-
-        Integer month = null;
-
-        Integer day = null;
-
-        Integer hour = ZERO;
-
-        Integer minute = ZERO;
-
-        Integer second = ZERO;
-
-        Integer nanos = ZERO;
-
-        String timeZone;
-
-        public Context(String s) {
-            is = new LexerInputStream(s.toCharArray());
-        }
-
-        public State getState() {
-            return state;
-        }
-
-        public void setState(State state) {
-            this.state = state;
-        }
-
-        public int read() throws IOException {
-            return is.read();
-        }
-
-        public void unread() throws IOException {
-            is.unread();
-        }
-
-        public void mark() {
-            is.mark();
-        }
-
-        public void reset() {
-            is.reset();
-        }
-
-        public boolean available() throws IOException {
-            return is.ready();
-        }
-
-    }
-
-    private interface Verifier<T> {
-
-        void verify(T value) throws ASN1RuntimeException;
-
-    }
-
-    private class LessEqualVerifiyer implements Verifier<Integer> {
-
-        private String valueName;
-
-        private int threshold;
-
-        public LessEqualVerifiyer(String valueName, int threshold) {
-            this.valueName = valueName;
-            this.threshold = threshold;
-        }
-
-        @Override
-        public void verify(Integer value) throws ASN1RuntimeException {
-            if (value > threshold) {
-                throw new ASN1RuntimeException("Invalid " + valueName + ": " + value);
-            }
-        }
     }
 
 }
