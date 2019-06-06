45a71a7505cac22ff6aef42db8981f791cb7b15d
me@eskaton.ch <me@eskaton.ch>
2019-06-06 22:26:24 +0200

    Added tests for addAdditionalItems() and getNextNumber()

--- asn4j-compiler/src/test/java/ch/eskaton/asn4j/compiler/EnumeratedTypeCompilerTest.java
+++ asn4j-compiler/src/test/java/ch/eskaton/asn4j/compiler/EnumeratedTypeCompilerTest.java
@@ -27,40 +27,85 @@
 
 package ch.eskaton.asn4j.compiler;
 
+import ch.eskaton.asn4j.compiler.EnumeratedTypeCompiler.EnumerationItems;
 import ch.eskaton.asn4j.parser.ast.EnumerationItemNode;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static ch.eskaton.asn4j.parser.NoPosition.NO_POSITION;
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.junit.matchers.JUnitMatchers.containsString;
 import static org.mockito.Mockito.mock;
 
 public class EnumeratedTypeCompilerTest {
 
     @Test
     public void testAddRootItems() {
+        CompilerContext context = mock(CompilerContext.class);
         EnumeratedTypeCompiler compiler = new EnumeratedTypeCompiler();
-        EnumeratedTypeCompiler.EnumerationItems items = new EnumeratedTypeCompiler.EnumerationItems();
-        List<EnumerationItemNode> nodes = Arrays.asList(createNode("a", 0), createNode("b"),
-                createNode("c", 3));
+        EnumerationItems items = new EnumerationItems();
+        List<EnumerationItemNode> nodes = asList(createNode("a", 0), createNode("b"), createNode("c", 3));
 
-        compiler.addRootItems(mock(CompilerContext.class), "type", items, nodes);
+        compiler.addRootItems(context, "type", items, nodes);
 
         assertEquals(0, (int) items.getNumbers().get(0));
         assertEquals(1, (int) items.getNumbers().get(1));
         assertEquals(3, (int) items.getNumbers().get(2));
 
-        items = new EnumeratedTypeCompiler.EnumerationItems();
-        nodes = Arrays.asList(createNode("a", -2), createNode("b"),
-                createNode("c", 0));
+        items = new EnumerationItems();
+        nodes = asList(createNode("a", -2), createNode("b"), createNode("c", 0));
 
-        compiler.addRootItems(mock(CompilerContext.class), "type", items, nodes);
+        compiler.addRootItems(context, "type", items, nodes);
 
         assertEquals(-2, (int) items.getNumbers().get(0));
         assertEquals(1, (int) items.getNumbers().get(1));
         assertEquals(0, (int) items.getNumbers().get(2));
+    }
+
+    @Test
+    public void testAdditionalItems() {
+        CompilerContext context = mock(CompilerContext.class);
+        EnumeratedTypeCompiler compiler = new EnumeratedTypeCompiler();
+        EnumerationItems items = new EnumerationItems();
+        List<EnumerationItemNode> rootNodes = asList(createNode("a", 0), createNode("b"), createNode("c", 3));
+
+        compiler.addRootItems(context, "type", items, rootNodes);
+
+        List<EnumerationItemNode> additionalNodes = asList(createNode("d"), createNode("e", 5), createNode("f", 7),
+                createNode("g"));
+
+        compiler.addAdditionalItems(context, "type", items, additionalNodes);
+
+        assertEquals(4, (int) items.getNumbers().get(3));
+        assertEquals(5, (int) items.getNumbers().get(4));
+        assertEquals(7, (int) items.getNumbers().get(5));
+        assertEquals(8, (int) items.getNumbers().get(6));
+
+        items = new EnumerationItems();
+
+        compiler.addRootItems(context, "type", items, rootNodes);
+
+        additionalNodes = asList(createNode("d"), createNode("e", 4));
+
+        try {
+            compiler.addAdditionalItems(context, "type", items, additionalNodes);
+            fail("CompilerException expected");
+        } catch (CompilerException e) {
+            assertThat(e.getMessage(), containsString("Duplicate enumeration value"));
+        }
+    }
+
+    @Test
+    public void testGetNextNumber() {
+        EnumeratedTypeCompiler compiler = new EnumeratedTypeCompiler();
+
+        assertEquals(4, (int) compiler.getNextNumber(asList(1, 2, 3), 3));
+        assertEquals(3, (int) compiler.getNextNumber(asList(1, 2, 3), 2));
+        assertEquals(10, (int) compiler.getNextNumber(asList(1, 9, 2, 5), 3));
     }
 
     private EnumerationItemNode createNode(String name) {
