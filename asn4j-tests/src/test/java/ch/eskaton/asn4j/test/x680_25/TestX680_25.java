81100dfcb6872f61df0702e2722a232ab07874e4
me@eskaton.ch <me@eskaton.ch>
2020-05-17 22:28:01 +0200

    Fixed bug: Generate extension additions; improved tests

--- asn4j-tests/src/test/java/ch/eskaton/asn4j/test/x680_25/TestX680_25.java
+++ asn4j-tests/src/test/java/ch/eskaton/asn4j/test/x680_25/TestX680_25.java
@@ -30,28 +30,37 @@ package ch.eskaton.asn4j.test.x680_25;
 import ch.eskaton.asn4j.runtime.types.ASN1Boolean;
 import ch.eskaton.asn4j.runtime.types.ASN1Integer;
 import ch.eskaton.asn4j.runtime.types.ASN1OctetString;
-import ch.eskaton.asn4j.test.TestHelper;
 import ch.eskaton.asn4j.test.modules.x680_25.TestSequence0;
 import ch.eskaton.asn4j.test.modules.x680_25.TestSequence1;
+import ch.eskaton.asn4j.test.modules.x680_25.TestSequence10;
+import ch.eskaton.asn4j.test.modules.x680_25.TestSequence11;
 import ch.eskaton.asn4j.test.modules.x680_25.TestSequence2;
 import ch.eskaton.asn4j.test.modules.x680_25.TestSequence3;
 import ch.eskaton.asn4j.test.modules.x680_25.TestSequence4;
+import ch.eskaton.asn4j.test.modules.x680_25.TestSequence5;
 import ch.eskaton.asn4j.test.modules.x680_25.TestSequence6;
+import ch.eskaton.asn4j.test.modules.x680_25.TestSequence7;
 import ch.eskaton.asn4j.test.modules.x680_25.TestSequence8;
 import ch.eskaton.asn4j.test.modules.x680_25.TestSequence9;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static ch.eskaton.asn4j.test.TestHelper.assertDecodable;
+import static ch.eskaton.asn4j.test.TestHelper.assertDecodableVerifyAfter;
+import static ch.eskaton.commons.utils.Utils.with;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DisplayName("X.680 25 - SEQUENCE tests")
 public class TestX680_25 {
 
     @Test
+    @DisplayName("Single component")
     public void testSequence0() {
         assertDecodable(TestSequence0.class, value -> value.setA(ASN1Boolean.TRUE));
     }
 
     @Test
+    @DisplayName("Multiple components")
     public void testSequence1() {
         assertDecodable(TestSequence1.class, value -> {
             value.setA(ASN1Integer.valueOf(4711));
@@ -61,7 +70,8 @@ public class TestX680_25 {
     }
 
     @Test
-    public void testOptional() {
+    @DisplayName("Optional component")
+    public void testSequence2() {
         assertDecodable(TestSequence2.class, value -> {
             value.setA(ASN1Integer.valueOf(4711));
             value.setC(ASN1OctetString.valueOf(new byte[] { (byte) 0xff, (byte) 0x56 }));
@@ -69,8 +79,9 @@ public class TestX680_25 {
     }
 
     @Test
-    public void testDefaultAtEnd() {
-        TestHelper.assertDecodableVerifyAfter(TestSequence3.class,
+    @DisplayName("Default values at end")
+    public void testSequence3() {
+        assertDecodableVerifyAfter(TestSequence3.class,
                 value -> {
                     value.setA(ASN1Integer.valueOf(4711));
                     value.setB(ASN1Boolean.TRUE);
@@ -79,8 +90,9 @@ public class TestX680_25 {
     }
 
     @Test
-    public void testDefaultAtStart() {
-        TestHelper.assertDecodableVerifyAfter(TestSequence4.class,
+    @DisplayName("Default values at beginning")
+    public void testSequence4() {
+        assertDecodableVerifyAfter(TestSequence4.class,
                 value -> {
                     value.setB(ASN1Boolean.TRUE);
                     value.setC(ASN1OctetString.valueOf(new byte[] { (byte) 0xff, (byte) 0x56 }));
@@ -89,36 +101,118 @@ public class TestX680_25 {
     }
 
     @Test
-    public void testComponentsOf() {
-        assertDecodable(TestSequence6.class, value -> {
-            value.setA(ASN1Integer.valueOf(4711));
-            value.setB(ASN1Boolean.TRUE);
-        });
+    @DisplayName("Extension addition")
+    public void testSequence5() {
+        assertDecodableVerifyAfter(TestSequence5.class,
+                value -> {
+                    value.setA(ASN1Integer.valueOf(4711));
+                    value.setZ(ASN1Integer.valueOf(23));
+                    value.setB(ASN1Boolean.TRUE);
+                },
+                value -> {
+                    assertEquals(ASN1Integer.valueOf(4711), value.getA());
+                    assertEquals(ASN1Integer.valueOf(23), value.getZ());
+                    assertEquals(ASN1Boolean.TRUE, value.getB());
+                });
     }
 
     @Test
-    public void testComponentsOfDefault() {
-        TestHelper.assertDecodableVerifyAfter(TestSequence6.class,
-                value -> value.setB(ASN1Boolean.TRUE),
-                value -> assertEquals(ASN1Integer.valueOf(23), value.getA()));
+    @DisplayName("Include components with COMPONENTS OF")
+    public void testSequence6() {
+        assertDecodableVerifyAfter(TestSequence6.class, value -> {
+                    value.setA(ASN1Integer.valueOf(4711));
+                    value.setB(ASN1Boolean.TRUE);
+                    value.setC(ASN1Boolean.FALSE);
+                },
+                value -> {
+                    assertEquals(ASN1Integer.valueOf(4711), value.getA());
+                    assertEquals(ASN1Boolean.TRUE, value.getB());
+                    assertEquals(ASN1Boolean.FALSE, value.getC());
+                });
     }
 
     @Test
+    @DisplayName("Include components with COMPONENTS OF with default value")
+    public void testSequence6defaults() {
+        assertDecodableVerifyAfter(TestSequence6.class,
+                value -> {
+                    value.setB(ASN1Boolean.FALSE);
+                    value.setC(ASN1Boolean.TRUE);
+                },
+                value -> {
+                    assertEquals(ASN1Integer.valueOf(23), value.getA());
+                    assertEquals(ASN1Boolean.FALSE, value.getB());
+                    assertEquals(ASN1Boolean.TRUE, value.getC());
+                });
+    }
+
+    @Test
+    @DisplayName("Extension groups with version")
+    public void testSequence7() {
+        assertDecodableVerifyAfter(TestSequence7.class,
+                value -> {
+                    value.setA(ASN1Integer.valueOf(23));
+                    value.setC(ASN1Integer.valueOf(97));
+                    value.setD(ASN1Boolean.TRUE);
+                    value.setB(ASN1Integer.valueOf(4711));
+                },
+                value -> {
+                    assertEquals(ASN1Integer.valueOf(23), value.getA());
+                    assertEquals(ASN1Integer.valueOf(97), value.getC());
+                    assertEquals(ASN1Boolean.TRUE, value.getD());
+                    assertEquals(ASN1Integer.valueOf(4711), value.getB());
+                });
+    }
+
+    @Test
+    @DisplayName("Nested sequence")
     public void testSequence8() {
-        var childValue = new TestSequence8.ChildSequence();
-
-        childValue.setA(ASN1Integer.valueOf(4711L));
-
-        assertDecodable(TestSequence8.class, value -> value.setChildSequence(childValue));
+        assertDecodableVerifyAfter(TestSequence8.class,
+                value -> value.setChildSequence(with(new TestSequence8.ChildSequence(),
+                        childValue -> childValue.setA(ASN1Integer.valueOf(4711)))),
+                value -> assertEquals(ASN1Integer.valueOf(4711), value.getChildSequence().getA()));
     }
 
     @Test
+    @DisplayName("Enumerated component")
     public void testSequence9() {
-        var childValue = new TestSequence9.ChildEnumerated();
+        assertDecodableVerifyAfter(TestSequence9.class,
+                value -> value.setChildEnumerated(with(new TestSequence9.ChildEnumerated(),
+                        childValue -> childValue.setValue(TestSequence9.ChildEnumerated.A))),
+                value -> assertEquals(TestSequence9.ChildEnumerated.A, value.getChildEnumerated()));
+    }
 
-        childValue.setValue(TestSequence9.ChildEnumerated.A);
+    @Test
+    @DisplayName("Extension group without version")
+    public void testSequence10() {
+        assertDecodableVerifyAfter(TestSequence10.class,
+                value -> {
+                    value.setA(ASN1Integer.valueOf(23));
+                    value.setC(ASN1Integer.valueOf(97));
+                    value.setD(ASN1Boolean.TRUE);
+                    value.setB(ASN1Integer.valueOf(4712));
+                },
+                value -> {
+                    assertEquals(ASN1Integer.valueOf(23), value.getA());
+                    assertEquals(ASN1Integer.valueOf(97), value.getC());
+                    assertEquals(ASN1Boolean.TRUE, value.getD());
+                    assertEquals(ASN1Integer.valueOf(4712), value.getB());
+                });
+    }
 
-        assertDecodable(TestSequence9.class, value -> value.setChildEnumerated(childValue));
+    @Test
+    @DisplayName("Nested sequence with extension addition")
+    public void testSequence11() {
+        assertDecodableVerifyAfter(TestSequence11.class,
+                value -> value.setChildSequence(with(new TestSequence11.ChildSequence(),
+                        childValue -> {
+                            childValue.setA(ASN1Integer.valueOf(4711));
+                            childValue.setB(ASN1Boolean.TRUE);
+                        })),
+                value -> {
+                    assertEquals(ASN1Integer.valueOf(4711), value.getChildSequence().getA());
+                    assertEquals(ASN1Boolean.TRUE, value.getChildSequence().getB());
+                });
     }
 
 }
