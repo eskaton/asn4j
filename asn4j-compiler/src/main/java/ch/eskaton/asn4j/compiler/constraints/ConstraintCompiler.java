4c199a1bc797c28fcef7be17fbcf8dab8e374b87
me@eskaton.ch <me@eskaton.ch>
2020-07-10 00:20:13 +0200

    Added constraint compilers to type definitions

--- asn4j-compiler/src/main/java/ch/eskaton/asn4j/compiler/constraints/ConstraintCompiler.java
+++ asn4j-compiler/src/main/java/ch/eskaton/asn4j/compiler/constraints/ConstraintCompiler.java
@@ -35,84 +35,21 @@ import ch.eskaton.asn4j.compiler.il.Module;
 import ch.eskaton.asn4j.compiler.java.objs.JavaClass;
 import ch.eskaton.asn4j.compiler.results.CompiledType;
 import ch.eskaton.asn4j.parser.ast.constraints.Constraint;
-import ch.eskaton.asn4j.parser.ast.types.BMPString;
-import ch.eskaton.asn4j.parser.ast.types.BitString;
-import ch.eskaton.asn4j.parser.ast.types.BooleanType;
-import ch.eskaton.asn4j.parser.ast.types.Choice;
-import ch.eskaton.asn4j.parser.ast.types.EnumeratedType;
-import ch.eskaton.asn4j.parser.ast.types.GeneralString;
-import ch.eskaton.asn4j.parser.ast.types.GraphicString;
-import ch.eskaton.asn4j.parser.ast.types.IA5String;
-import ch.eskaton.asn4j.parser.ast.types.IRI;
-import ch.eskaton.asn4j.parser.ast.types.ISO646String;
-import ch.eskaton.asn4j.parser.ast.types.IntegerType;
-import ch.eskaton.asn4j.parser.ast.types.Null;
-import ch.eskaton.asn4j.parser.ast.types.NumericString;
-import ch.eskaton.asn4j.parser.ast.types.ObjectIdentifier;
-import ch.eskaton.asn4j.parser.ast.types.OctetString;
-import ch.eskaton.asn4j.parser.ast.types.PrintableString;
-import ch.eskaton.asn4j.parser.ast.types.RelativeIRI;
-import ch.eskaton.asn4j.parser.ast.types.RelativeOID;
-import ch.eskaton.asn4j.parser.ast.types.SequenceOfType;
-import ch.eskaton.asn4j.parser.ast.types.SequenceType;
-import ch.eskaton.asn4j.parser.ast.types.SetOfType;
-import ch.eskaton.asn4j.parser.ast.types.SetType;
-import ch.eskaton.asn4j.parser.ast.types.T61String;
-import ch.eskaton.asn4j.parser.ast.types.TeletexString;
 import ch.eskaton.asn4j.parser.ast.types.Type;
-import ch.eskaton.asn4j.parser.ast.types.UTF8String;
-import ch.eskaton.asn4j.parser.ast.types.UniversalString;
-import ch.eskaton.asn4j.parser.ast.types.VideotexString;
-import ch.eskaton.asn4j.parser.ast.types.VisibleString;
 import ch.eskaton.asn4j.runtime.exceptions.ConstraintViolatedException;
-import ch.eskaton.commons.collections.Maps;
 import ch.eskaton.commons.collections.Tuple2;
 
-import java.util.Map;
 import java.util.Optional;
 
 import static java.util.Collections.singletonList;
 
 public class ConstraintCompiler {
 
-    private Map<Class<? extends Type>, AbstractConstraintCompiler> compilers;
-
     private CompilerContext ctx;
 
     @SuppressWarnings("serial")
     public ConstraintCompiler(CompilerContext ctx) {
         this.ctx = ctx;
-
-        compilers = Maps.<Class<? extends Type>, AbstractConstraintCompiler>builder()
-                .put(BooleanType.class, new BooleanConstraintCompiler(ctx))
-                .put(IntegerType.class, new IntegerConstraintCompiler(ctx))
-                .put(EnumeratedType.class, new EnumeratedTypeConstraintCompiler(ctx))
-                .put(BitString.class, new BitStringConstraintCompiler(ctx))
-                .put(Null.class, new NullConstraintCompiler(ctx))
-                .put(ObjectIdentifier.class, new ObjectIdentifierConstraintCompiler(ctx))
-                .put(OctetString.class, new OctetStringConstraintCompiler(ctx))
-                .put(RelativeOID.class, new RelativeOIDConstraintCompiler(ctx))
-                .put(IRI.class, new IRIConstraintCompiler(ctx))
-                .put(RelativeIRI.class, new RelativeIRIConstraintCompiler(ctx))
-                .put(SetOfType.class, new SetOfConstraintCompiler(ctx))
-                .put(SequenceOfType.class, new SequenceOfConstraintCompiler(ctx))
-                .put(SetType.class, new SetConstraintCompiler(ctx))
-                .put(SequenceType.class, new SequenceConstraintCompiler(ctx))
-                .put(Choice.class, new ChoiceConstraintCompiler(ctx))
-                .put(VisibleString.class, new VisibleStringConstraintCompiler(ctx))
-                .put(ISO646String.class, new VisibleStringConstraintCompiler(ctx))
-                .put(GeneralString.class, new GeneralStringConstraintCompiler(ctx))
-                .put(GraphicString.class, new GraphicStringConstraintCompiler(ctx))
-                .put(IA5String.class, new IA5StringConstraintCompiler(ctx))
-                .put(VideotexString.class, new VideotexStringConstraintCompiler(ctx))
-                .put(TeletexString.class, new TeletexStringConstraintCompiler(ctx))
-                .put(T61String.class, new TeletexStringConstraintCompiler(ctx))
-                .put(PrintableString.class, new PrintableStringConstraintCompiler(ctx))
-                .put(NumericString.class, new NumericStringConstraintCompiler(ctx))
-                .put(UTF8String.class, new UTF8StringConstraintCompiler(ctx))
-                .put(UniversalString.class, new UniversalStringConstraintCompiler(ctx))
-                .put(BMPString.class, new BMPStringConstraintCompiler(ctx))
-                .build();
     }
 
     public ConstraintDefinition compileConstraint(JavaClass javaClass, String name, CompiledType compiledType) {
@@ -138,12 +75,7 @@ public class ConstraintCompiler {
     }
 
     private AbstractConstraintCompiler getCompiler(CompiledType compiledType) {
-        if (!compilers.containsKey(compiledType.getType().getClass())) {
-            throw new CompilerException("Constraints for type %s not yet supported",
-                    compiledType.getType().getClass().getSimpleName());
-        }
-
-        return compilers.get(compiledType.getType().getClass());
+        return ctx.getConstraintCompiler(compiledType.getType().getClass());
     }
 
     public ConstraintDefinition compileConstraint(CompiledType compiledType) {
