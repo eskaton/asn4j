673f529781921608bd8e7fa83bda2ad6d4acc707
me@eskaton.ch <me@eskaton.ch>
2020-09-14 11:00:22 +0200

    Added missing methods

--- asn4j-compiler/src/main/java/ch/eskaton/asn4j/compiler/results/CompiledParameterizedObjectClass.java
+++ asn4j-compiler/src/main/java/ch/eskaton/asn4j/compiler/results/CompiledParameterizedObjectClass.java
@@ -48,4 +48,33 @@ public class CompiledParameterizedObjectClass extends AbstractCompiledParameteri
         return objectClass;
     }
 
+    @Override
+    public String toString() {
+        return ToString.get(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        CompiledParameterizedObjectClass that = (CompiledParameterizedObjectClass) o;
+
+        return Objects.equals(objectClass, that.objectClass);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), objectClass);
+    }
+
 }
