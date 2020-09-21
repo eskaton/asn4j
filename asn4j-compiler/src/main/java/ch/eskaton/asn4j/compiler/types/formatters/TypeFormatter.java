f97715ab63ac5fd8e10fde37c9ea4f226271c93f
me@eskaton.ch <me@eskaton.ch>
2020-09-21 17:42:37 +0200

    Use dispatcher in TypeFormatter

--- asn4j-compiler/src/main/java/ch/eskaton/asn4j/compiler/types/formatters/TypeFormatter.java
+++ asn4j-compiler/src/main/java/ch/eskaton/asn4j/compiler/types/formatters/TypeFormatter.java
@@ -28,13 +28,13 @@
 package ch.eskaton.asn4j.compiler.types.formatters;
 
 import ch.eskaton.asn4j.compiler.CompilerContext;
+import ch.eskaton.asn4j.compiler.CompilerException;
 import ch.eskaton.asn4j.compiler.IllegalCompilerStateException;
 import ch.eskaton.asn4j.parser.ast.Node;
 import ch.eskaton.asn4j.parser.ast.types.BMPString;
 import ch.eskaton.asn4j.parser.ast.types.BitString;
 import ch.eskaton.asn4j.parser.ast.types.BooleanType;
 import ch.eskaton.asn4j.parser.ast.types.Choice;
-import ch.eskaton.asn4j.parser.ast.types.CollectionOfType;
 import ch.eskaton.asn4j.parser.ast.types.EnumeratedType;
 import ch.eskaton.asn4j.parser.ast.types.GeneralString;
 import ch.eskaton.asn4j.parser.ast.types.GraphicString;
@@ -55,11 +55,16 @@ import ch.eskaton.asn4j.parser.ast.types.SetOfType;
 import ch.eskaton.asn4j.parser.ast.types.SetType;
 import ch.eskaton.asn4j.parser.ast.types.T61String;
 import ch.eskaton.asn4j.parser.ast.types.TeletexString;
+import ch.eskaton.asn4j.parser.ast.types.Type;
 import ch.eskaton.asn4j.parser.ast.types.TypeReference;
 import ch.eskaton.asn4j.parser.ast.types.UTF8String;
 import ch.eskaton.asn4j.parser.ast.types.UniversalString;
 import ch.eskaton.asn4j.parser.ast.types.VideotexString;
 import ch.eskaton.asn4j.parser.ast.types.VisibleString;
+import ch.eskaton.commons.collections.Tuple2;
+import ch.eskaton.commons.utils.Dispatcher;
+
+import java.util.function.BiFunction;
 
 import static ch.eskaton.asn4j.runtime.types.TypeName.BMP_STRING;
 import static ch.eskaton.asn4j.runtime.types.TypeName.GENERAL_STRING;
@@ -75,74 +80,61 @@ import static ch.eskaton.asn4j.runtime.types.TypeName.UNIVERSAL_STRING;
 import static ch.eskaton.asn4j.runtime.types.TypeName.UTF8_STRING;
 import static ch.eskaton.asn4j.runtime.types.TypeName.VIDEOTEX_STRING;
 import static ch.eskaton.asn4j.runtime.types.TypeName.VISIBLE_STRING;
+import static ch.eskaton.commons.utils.Utils.callWith;
 
-public class TypeFormatter<S extends CollectionOfType> {
+public class TypeFormatter {
+
+    public static final Dispatcher<Type, Class<? extends Type>, Tuple2<CompilerContext, ? extends Type>, String> DISPATCHER =
+            new Dispatcher<Type, Class<? extends Type>, Tuple2<CompilerContext, ? extends Type>, String>()
+                    .withComparator((t, u) -> u.isInstance(t))
+                    .withException(t -> new CompilerException("Formatter for type %s not defined", t));
+
+    static {
+        addCase(BooleanType.class, new BooleanFormatter()::format);
+        addCase(IntegerType.class, new IntegerFormatter()::format);
+        addCase(EnumeratedType.class, new EnumeratedFormatter()::format);
+        addCase(BitString.class, new BitStringFormatter()::format);
+        addCase(OctetString.class, new OctetStringFormatter()::format);
+        addCase(Null.class, new DefaultTypeFormatter<Null>(NULL)::format);
+        addCase(ObjectIdentifier.class, new ObjectIdentifierFormatter()::format);
+        addCase(RelativeOID.class, new RelativeOIDFormatter()::format);
+        addCase(IRI.class, new IRIFormatter()::format);
+        addCase(RelativeIRI.class, new RelativeIRIFormatter()::format);
+        addCase(SequenceType.class, new SequenceFormatter()::format);
+        addCase(SetType.class, new SetFormatter()::format);
+        addCase(SequenceOfType.class, new SequenceOfFormatter()::format);
+        addCase(SetOfType.class, new SetOfFormatter()::format);
+        addCase(Choice.class, new ChoiceFormatter()::format);
+        addCase(VisibleString.class, new DefaultTypeFormatter<VisibleString>(VISIBLE_STRING)::format);
+        addCase(ISO646String.class, new DefaultTypeFormatter<ISO646String>(ISO646_STRING)::format);
+        addCase(NumericString.class, new DefaultTypeFormatter<NumericString>(NUMERIC_STRING)::format);
+        addCase(PrintableString.class, new DefaultTypeFormatter<PrintableString>(PRINTABLE_STRING)::format);
+        addCase(IA5String.class, new DefaultTypeFormatter<IA5String>(IA5_STRING)::format);
+        addCase(GraphicString.class, new DefaultTypeFormatter<GraphicString>(GRAPHIC_STRING)::format);
+        addCase(GeneralString.class, new DefaultTypeFormatter<GeneralString>(GENERAL_STRING)::format);
+        addCase(TeletexString.class, new DefaultTypeFormatter<TeletexString>(TELETEX_STRING)::format);
+        addCase(T61String.class, new DefaultTypeFormatter<T61String>(T61_STRING)::format);
+        addCase(VideotexString.class, new DefaultTypeFormatter<VideotexString>(VIDEOTEX_STRING)::format);
+        addCase(UTF8String.class, new DefaultTypeFormatter<UTF8String>(UTF8_STRING)::format);
+        addCase(UniversalString.class, new DefaultTypeFormatter<UniversalString>(UNIVERSAL_STRING)::format);
+        addCase(BMPString.class, new DefaultTypeFormatter<BMPString>(BMP_STRING)::format);
+        addCase(TypeReference.class, new TypeReferenceFormatter()::format);
+    }
 
     private TypeFormatter() {
     }
 
     public static String formatType(CompilerContext ctx, Node node) {
-        if (node instanceof SequenceType type) {
-            return new SequenceFormatter().format(ctx, type);
-        } else if (node instanceof SetType type) {
-            return new SetTypeFormatter().format(ctx, type);
-        } else if (node instanceof SequenceOfType type) {
-            return new SequenceOfFormatter().format(ctx, type);
-        } else if (node instanceof SetOfType type) {
-            return new SetOfFormatter().format(ctx, type);
-        } else if (node instanceof Choice type) {
-            return new ChoiceFormatter().format(ctx, type);
-        } else if (node instanceof BooleanType type) {
-            return new BooleanFormatter().format(ctx, type);
-        } else if (node instanceof IntegerType type) {
-            return new IntegerFormatter().format(ctx, type);
-        } else if (node instanceof EnumeratedType type) {
-            return new EnumeratedFormatter().format(ctx, type);
-        } else if (node instanceof BitString type) {
-            return new BitStringFormatter().format(ctx, type);
-        } else if (node instanceof OctetString type) {
-            return new OctetStringFormatter().format(ctx, type);
-        } else if (node instanceof Null type) {
-            return new DefaultTypeFormatter<Null>(NULL).format(ctx, type);
-        } else if (node instanceof ObjectIdentifier type) {
-            return new ObjectIdentifierFormatter().format(ctx, type);
-        } else if (node instanceof RelativeOID type) {
-            return new RelativeOIDFormatter().format(ctx, type);
-        } else if (node instanceof IRI type) {
-            return new IRIFormatter().format(ctx, type);
-        } else if (node instanceof RelativeIRI type) {
-            return new RelativeIRIFormatter().format(ctx, type);
-        } else if (node instanceof VisibleString type) {
-            return new DefaultTypeFormatter<VisibleString>(VISIBLE_STRING).format(ctx, type);
-        } else if (node instanceof ISO646String type) {
-            return new DefaultTypeFormatter<ISO646String>(ISO646_STRING).format(ctx, type);
-        } else if (node instanceof NumericString type) {
-            return new DefaultTypeFormatter<NumericString>(NUMERIC_STRING).format(ctx, type);
-        } else if (node instanceof PrintableString type) {
-            return new DefaultTypeFormatter<PrintableString>(PRINTABLE_STRING).format(ctx, type);
-        } else if (node instanceof IA5String type) {
-            return new DefaultTypeFormatter<IA5String>(IA5_STRING).format(ctx, type);
-        } else if (node instanceof GraphicString type) {
-            return new DefaultTypeFormatter<GraphicString>(GRAPHIC_STRING).format(ctx, type);
-        } else if (node instanceof GeneralString type) {
-            return new DefaultTypeFormatter<GeneralString>(GENERAL_STRING).format(ctx, type);
-        } else if (node instanceof TeletexString type) {
-            return new DefaultTypeFormatter<TeletexString>(TELETEX_STRING).format(ctx, type);
-        } else if (node instanceof T61String type) {
-            return new DefaultTypeFormatter<T61String>(T61_STRING).format(ctx, type);
-        } else if (node instanceof VideotexString type) {
-            return new DefaultTypeFormatter<VideotexString>(VIDEOTEX_STRING).format(ctx, type);
-        } else if (node instanceof UTF8String type) {
-            return new DefaultTypeFormatter<UTF8String>(UTF8_STRING).format(ctx, type);
-        } else if (node instanceof UniversalString type) {
-            return new DefaultTypeFormatter<UniversalString>(UNIVERSAL_STRING).format(ctx, type);
-        } else if (node instanceof BMPString type) {
-            return new DefaultTypeFormatter<BMPString>(BMP_STRING).format(ctx, type);
-        } else if (node instanceof TypeReference type) {
-            return new TypeReferenceFormatter().format(ctx, type);
+        if (node instanceof Type type) {
+            return DISPATCHER.execute(type, Tuple2.of(ctx, type));
         }
 
-        throw new IllegalCompilerStateException("Formatter for node %s not defined", node.getClass());
+        throw new IllegalCompilerStateException("Invalid type: %s", node.getClass().getSimpleName());
+    }
+
+    private static <T extends Type> void addCase(Class<T> typeClass, BiFunction<CompilerContext, T, String> formatter) {
+        DISPATCHER.withCase(typeClass,
+                maybeArgs -> callWith(args -> formatter.apply(args.get_1(), (T) args.get_2()), maybeArgs.get()));
     }
 
 }
