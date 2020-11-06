8e172925813a63ad7c80ec58db8188faa03203a2
me@eskaton.ch <me@eskaton.ch>
2020-11-06 23:39:59 +0200

    Resolve value from object

--- asn4j-compiler/src/main/java/ch/eskaton/asn4j/compiler/values/AbstractValueCompiler.java
+++ asn4j-compiler/src/main/java/ch/eskaton/asn4j/compiler/values/AbstractValueCompiler.java
@@ -36,12 +36,15 @@ import ch.eskaton.asn4j.compiler.results.CompiledType;
 import ch.eskaton.asn4j.compiler.results.CompiledValue;
 import ch.eskaton.asn4j.compiler.types.formatters.TypeFormatter;
 import ch.eskaton.asn4j.compiler.values.formatters.ValueFormatter;
+import ch.eskaton.asn4j.parser.ast.ObjectReference;
 import ch.eskaton.asn4j.parser.ast.values.DefinedValue;
 import ch.eskaton.asn4j.parser.ast.values.ExternalValueReference;
 import ch.eskaton.asn4j.parser.ast.values.SimpleDefinedValue;
 import ch.eskaton.asn4j.parser.ast.values.Value;
+import ch.eskaton.asn4j.parser.ast.values.ValueFromObject;
 import ch.eskaton.asn4j.runtime.types.TypeName;
 
+import java.util.Map;
 import java.util.Optional;
 
 public abstract class AbstractValueCompiler<V extends Value> implements Compiler<V> {
@@ -125,11 +128,63 @@ public abstract class AbstractValueCompiler<V extends Value> implements Compiler
     @SuppressWarnings("unused")
     public V doCompile(CompilerContext ctx, CompiledType compiledType, Value value,
             Optional<Parameters> maybeParameters) {
+        value = resolveValueFromObject(ctx, value);
+
         if (value.getClass().isAssignableFrom(valueClass)) {
             return (V) value;
         }
 
         throw invalidValueError(value);
+    }
+
+    protected Value resolveValueFromObject(CompilerContext ctx, Value value) {
+        if (!(value instanceof ValueFromObject)) {
+            return value;
+        }
+
+        var valueFromObject = (ValueFromObject) value;
+        var referencedObjects = valueFromObject.getReference();
+
+        if (!(referencedObjects instanceof ObjectReference)) {
+            var formattedValue = ValueFormatter.formatValue(value);
+
+            throw new IllegalCompilerStateException(value.getPosition(), "%s contains an invalid object reference",
+                    formattedValue);
+        }
+
+        var objectReference = (ObjectReference) referencedObjects;
+        var compiledObject = ctx.getCompiledObject(objectReference);
+        var objectDefinition = compiledObject.getObjectDefinition();
+        var fieldNames = valueFromObject.getField().getPrimitiveFieldNames();
+        Object resolvedValue = null;
+
+        for (var fieldName : fieldNames) {
+            var fieldReference = fieldName.getReference();
+
+            if (!fieldName.isValueFieldReference() || objectDefinition == null ||
+                    !objectDefinition.containsKey(fieldReference)) {
+                var formattedValue = ValueFormatter.formatValue(valueFromObject);
+
+                throw new CompilerException(fieldName.getPosition(), "%s in %s doesn't refer to a value",
+                        fieldName.getReference(), formattedValue);
+            }
+
+            resolvedValue = objectDefinition.get(fieldReference);
+
+            if (resolvedValue instanceof Map) {
+                objectDefinition = (Map<String, Object>) resolvedValue;
+            } else {
+                objectDefinition = null;
+            }
+        }
+
+        if (!(value instanceof Value)) {
+            var formattedValue = ValueFormatter.formatValue(valueFromObject);
+
+            throw new CompilerException(valueFromObject.getPosition(), "%s doesn't refer to a value", formattedValue);
+        }
+
+        return (Value) resolvedValue;
     }
 
     protected CompilerException invalidValueError(Value value) {
